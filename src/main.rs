ba62b76fe27580611a20bd7360dcf708ea06a5d0
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-02-24 08:23:07 +0100

    bug: remove fib function added a unit type fucntion

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-use std::io;
-
-
-
-
 fn main() {
-    
-    fn fib(num: u128) -> u128 {// 011
-
-        if num==0 {
-            return 0;
-        }else if num==1 {
-            return 1;
-        }else {
-            return fib(num-1) + fib(num - 2)
-        }
-    }
-
-    println!("Enter the number for the fibonacci: ");
-    let mut value = String::new();
-    io::stdin()
-    .read_line(&mut value)
-    .expect("Couldn't read the value");
-
-    let value: u128 = value.trim().parse().expect("Couldn't collect value");
-
-    println!("The fibonacci of {} is: {}", value, fib(value))
+    println!("Hello, welcome to my FibBot application")
 }
