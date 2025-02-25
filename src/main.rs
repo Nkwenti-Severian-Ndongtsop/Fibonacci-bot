b710a0e4120fd1821cd083ff305c1131ab1ef77f
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-02-25 08:28:21 +0100

    feat: added parameter collection, seting some defaults

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, welcome to my FibBot application")
+    
+    let args: Vec<String> = env::args().collect();
+
+    
+    let enable_fib = args.get(1).unwrap_or(&"true".to_string()).to_lowercase() == "true";
+    let max_threshold: u32 = args
+        .get(2)
+        .unwrap_or(&"100".to_string())
+        .parse()
+        .unwrap_or(100);
+
+    println!("FibBot application is running...");
+    println!("Fibonacci Calculation Enabled: {}", enable_fib);
+    println!("Max Threshold is: {}", max_threshold);
 }
